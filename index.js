6b69a1fd852f29f1d1e5e51dd00f5326b4ade5bd
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-01-19 01:12:44 +0530

    firs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
+
+
 server.use(middleware);
+
 server.use(router);
 server.listen(port);
